349c0f6d174056486107a2b3d83f7dacaddd546f
Christoph Gockel <github@christophgockel.de>
2014-03-01 23:30:21 +0100

    Polished terminal ui a bit

--- tictactoe.py
+++ tictactoe.py
@@ -1,40 +1,74 @@
 from game import *
+from board import make_board
 from player import *
 
 
 def user_input(player_symbol):
     while True:
-       input = raw_input('Next move for {0}:'.format(player_symbol))
+       input = raw_input('Next move for {0}: '.format(player_symbol))
        try:
            move = int(input)
 
-           if move >= 0 and move <= 8:
-               return move
+           if move >= 1 and move <= 9:
+               return move - 1
            else:
-               print 'Move has to be between 0 and 8. Please try again.'
+               print 'Move has to be between 1 and 9. Please try again.'
        except ValueError:
            print 'Not a valid move. Please try again.'
 
 def board_printer(board):
     index = 0
     for row in range(board.row_count()):
-        row_content = ''
-
-        for column in range(board.column_count()):
-            symbol = board[row, column]
-            if symbol is None:
-                symbol = index
-            row_content += '{0} '.format(symbol)
-            index += 1
-
+        row_content = ' | '.join([' ' if symbol is None else symbol for symbol in board.row(row)])
         print row_content
 
-player_x = Player('x', user_input)
-player_o = Player('o', user_input)
+        if row < board.row_count() - 1:
+            print '-' * len(row_content)
 
-board = Board(callback=board_printer)
-game = Game(board=board)
-game.add_player(player_x)
-game.add_player(player_o)
+def print_introduction():
+    print 'Tic Tac Toe'
+    print 'Play using the following coordinates:'
+    print_board_coordinates()
 
-game.run()
+def print_board_coordinates():
+    board_printer(make_board('123456789'))
+
+def print_outcome(outcome):
+    if outcome == GameState.tie:
+        print 'This game ended in a tie.'
+    elif outcome == GameState.winner_x:
+        print 'Player x is the winner.'
+    elif outcome == GameState.winner_o:
+        print 'Player o is the winner.'
+    else:
+        print 'Unknown outcome {0}.'.format(outcome)
+
+def player_is_done():
+    print 'Play another round?'
+    input = raw_input('If not, please enter "no", everything other means "yes": ')
+
+    if input.lower() == 'no':
+        return True
+    return False
+
+
+print_introduction()
+
+try:
+    while True:
+        player_x = Player('x', user_input)
+        player_o = Player('o', user_input)
+
+        board = Board(callback=board_printer)
+        game = Game(board=board)
+        game.add_player(player_x)
+        game.add_player(player_o)
+
+        outcome = game.run()
+        print_outcome(outcome)
+
+        if player_is_done():
+            break
+        print_board_coordinates()
+except KeyboardInterrupt:
+    pass
